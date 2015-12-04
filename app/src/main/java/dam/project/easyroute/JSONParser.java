638cb8aa9c413c928952010ef53315709ad79a5a
Theodor Gheghea <Theodor Gheghea>
2015-12-05 01:50:31 +0200

    Statiile se incarca intr-un listview cu aspect temporar; adaugat progressbar inaintea incarcarii listei

--- app/src/main/java/dam/project/easyroute/JSONParser.java
+++ app/src/main/java/dam/project/easyroute/JSONParser.java
@@ -1,6 +1,18 @@
 package dam.project.easyroute;
 
+import android.app.Application;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,24 +25,59 @@ import java.net.URL;
  * Created by Theo on 04.12.2015.
  */
 public class JSONParser {
-    public String descarcareJSONStatii() {
-        StringBuilder builder = new StringBuilder();
-        try {
-            URL url = new URL("http://urbo.ro/openmap_transport/load_nid");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            if (conn.getResponseCode() == 200) {
-                InputStream content = new BufferedInputStream(conn.getInputStream());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-            } else {
-                Log.e("JSONParser", "Nu s-a putut descarca fisierul");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return builder.toString();
+
+    public void incepeParsareJSON(ListView lv, ProgressBar pb)
+    {
+        new descarcareJSONTask().execute(lv, pb);
     }
+    private class descarcareJSONTask extends AsyncTask<View, Void, String>
+    {
+        private ListView lv;
+        private ProgressBar pb;
+        @Override
+        protected String doInBackground(View... views) {
+            lv = (ListView)views[0];
+            pb = (ProgressBar)views[1];
+            return descarcareJSONStatii();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject obiect = new JSONObject(s);
+                JSONArray listaStatii = obiect.getJSONObject("markers").getJSONArray("markers");
+                String statii[] = new String[listaStatii.length()];
+                for (int i = 0; i < listaStatii.length(); i++)
+                    statii[i] = listaStatii.getString(i);
+                ListAdapter adaptor = new ArrayAdapter<String>(lv.getContext(), android.R.layout.simple_list_item_1, statii);
+                lv.setAdapter(adaptor);
+                pb.setVisibility(View.GONE);
+                lv.setVisibility(View.VISIBLE);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String descarcareJSONStatii() {
+            StringBuilder builder = new StringBuilder();
+            try {
+                URL url = new URL("http://urbo.ro/openmap_transport/load_nid");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                if (conn.getResponseCode() == 200) {
+                    InputStream content = new BufferedInputStream(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+                } else {
+                    Log.e("JSONParser", "Nu s-a putut descarca fisierul");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return builder.toString();
+        }
+    }
+
 }
--- app/src/main/java/dam/project/easyroute/MainActivity.java
+++ app/src/main/java/dam/project/easyroute/MainActivity.java
@@ -2,6 +2,8 @@ package dam.project.easyroute;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv = (ListView)findViewById(R.id.statiiListView);
+        ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
+        JSONParser parser = new JSONParser();
+        parser.incepeParsareJSON(lv, pb);
     }
 }
